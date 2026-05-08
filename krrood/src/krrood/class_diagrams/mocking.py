65c268e3b635a2dde6fa06b275c0e8ff5899e928
AbdelrhmanBassiouny <abdelrhman_bassuny@hotmail.com>
2026-05-08 15:48:16 +0200

    [Pattern] review changes.

--- krrood/src/krrood/class_diagrams/mocking.py
+++ krrood/src/krrood/class_diagrams/mocking.py
@@ -6,14 +6,14 @@ from krrood.class_diagrams.exceptions import MockedClassInstantiationError
 @dataclass
 class MockedModule:
     """
-    Base class for mocking modules. Cannot be instantiated directly.
+    Base class for mocking modules.
     """
 
 
 @dataclass
 class MockedClass:
     """
-    Base class for mocking classes. Cannot be instantiated directly.
+    Base class for mocking classes. Cannot be instantiated (will raise an error).
     """
 
     def __new__(cls, *args, **kwargs):
--- pycram/src/pycram/motion_executor.py
+++ pycram/src/pycram/motion_executor.py
@@ -16,10 +16,7 @@ from giskardpy.motion_statechart.motion_statechart import (
     MotionStatechart,
 )
 from giskardpy.qp.qp_controller_config import QPControllerConfig
-try:
-    from giskardpy.ros_executor import Ros2Executor
-except ModuleNotFoundError as e:
-    Ros2Executor = None
+from giskardpy.ros_executor import Ros2Executor
 from pycram.datastructures.enums import ExecutionType
 from pycram.exceptions import MotionDidNotFinish
 
